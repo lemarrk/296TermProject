c4a089dc06611addd14077b867f9dd142c13e885
lemarrk80 <klemarr80@gmail.com>
2021-03-10 21:54:19 -0800

    added empty domain models for database

---
+++ KL296NTermProject/KL296NTermProject/Models/Topic.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KL296NTermProject.Models
+{
+    public class Topic
+    {
+    }
+}
